059fd9e08d00a80a38d29aeb4bc10bd07899d33e
Henrik Giesel <hengiesel@gmail.com>
2020-07-31 19:41:34 +0200

    Move more editor logic to hooks

--- src/editor.py
+++ src/editor.py
@@ -4,7 +4,7 @@ from aqt.editor import Editor
 from aqt.editcurrent import EditCurrent
 
 from aqt.utils import restoreGeom, saveGeom, tooltip
-from aqt.gui_hooks import editor_did_init
+from aqt.gui_hooks import editor_did_init, editor_did_init_shortcuts
 
 class PersistentEditor(Editor):
     def redrawMainWindow(self):
@@ -33,24 +33,12 @@ class PersistentEditor(Editor):
     def saveNowIfNecessary(self, callback, keepFocus=False):
         # avoid constant blinking on hover
         if hasattr(self, 'markChanged') and self.markChanged:
-            from aqt.utils import showText
-            showText('meh')
             self.saveNow(callback, keepFocus)
 
     # override methods
 
     def mungeHTML(self, txt):
         return super().mungeHTML(txt.replace('<div class="coverup"></div>', ''))
-
-    def onHtmlEdit(self):
-        field = self.currentField
-
-        def callback():
-            nonlocal field
-            self._onHtmlEdit(field)
-            self.maybeObscureAll()
-
-        self.saveNow(callback)
 
     def saveNow(self, callback, keepFocus=False):
         super().saveNow(callback, keepFocus)
@@ -117,12 +105,35 @@ class PersistentEditor(Editor):
         else:
             print("uncaught cmd", cmd)
 
-def setup_editor(editor):
+def alter_on_html(cuts, editor):
     if isinstance(editor.parentWindow, EditCurrent):
-        qconnect(editor.tags.lostFocus, editor.redrawMainWindow)
+        def onHtmlEdit_persistent():
+            field = editor.currentField
+
+            def callback():
+                nonlocal field
+                editor._onHtmlEdit(field)
+                editor.maybeObscureAll()
+                editor.redrawMainWindow()
+
+            editor.saveNow(callback)
+
+        try:
+            result = next(filter(lambda v: v[0] == 'Ctrl+Shift+X', cuts))
+        except StopIteration:
+            return
+
+        del cuts[cuts.index(result)]
+        cuts.append(('Ctrl+Shift+X', onHtmlEdit_persistent))
 
         # NOTE the markChanged property will identify this editor as being an editor within an editcurrent
         editor.markChanged = False
 
+def setup_editor(editor):
+    if isinstance(editor.parentWindow, EditCurrent):
+        qconnect(editor.tags.lostFocus, editor.redrawMainWindow)
+
 def init_editor():
+    # is executed before editor_did_init
+    editor_did_init_shortcuts.append(alter_on_html)
     editor_did_init.append(setup_editor)
