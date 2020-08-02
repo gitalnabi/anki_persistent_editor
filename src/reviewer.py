8dd45396b2765b1b2cdb49ec88a06aaaef85a3b4
Henrik Giesel <hengiesel@gmail.com>
2020-08-02 12:50:35 +0200

    Stop fading on question side after first time

--- src/reviewer.py
+++ src/reviewer.py
@@ -5,6 +5,7 @@ from aqt.reviewer import Reviewer
 from aqt.gui_hooks import (
     reviewer_will_end,
     reviewer_did_answer_card,
+    reviewer_did_show_question,
     card_will_show,
     webview_will_set_content,
 )
@@ -59,8 +60,16 @@ def setup_reviewer(web_content, context):
     if isinstance(context, Reviewer):
         context.triggerObscure = True
 
+def stop_fading_on_question(card):
+    mw.reviewer.web.eval('qFade = 0')
+
+def start_fading_again(reviewer, card, ease):
+    reviewer.web.eval('qFade = 100')
+
 def init_reviewer():
     card_will_show.append(obscure_editcurrent)
     reviewer_will_end.append(close_editcurrent)
-
     webview_will_set_content.append(setup_reviewer)
+
+    reviewer_did_show_question.append(stop_fading_on_question)
+    reviewer_did_answer_card.append(start_fading_again)
